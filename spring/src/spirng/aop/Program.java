ac0065b1ca528aa430fe1bb91aad201ae90dd442
likelake13 <likelake13@naver.com>
2021-10-20 22:48:48 +0900

    [이화수] Proxy를 사용해 aop 개념 활용해보기

---
+++ spring/src/spirng/aop/Program.java
@@ -0,0 +1,36 @@
+package spirng.aop;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import spring.aop.entity.Exam;
+import spring.aop.entity.NewlecExam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Exam exam = new NewlecExam(1,1,1,1);
+
+		Exam examProxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), new Class[] {Exam.class}, 
+				new InvocationHandler() {
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+				long start = System.currentTimeMillis();
+				
+				Object result = method.invoke(exam, args);
+				
+				long end = System.currentTimeMillis();
+				
+				String message = (end - start) + "ms 시간이 걸렸습니다.";
+				System.out.println(message);
+				return result;
+			}
+		});
+		
+		System.out.printf("total is %d\n", examProxy.total());
+		System.out.printf("total is %f\n", examProxy.avg());
+	}
+
+}
---
+++ spring/src/spring/aop/entity/NewlecExam.java
@@ -0,0 +1,90 @@
+package spring.aop.entity;
+
+/*import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+*/
+public class NewlecExam implements Exam {
+
+	private int kor;
+	private int math;
+	private int eng;
+	private int com;
+	
+	public NewlecExam() {
+		
+	}
+	
+	public NewlecExam(int kor, int math, int eng, int com) {
+		this.kor = kor;
+		this.math = math;
+		this.eng = eng;
+		this.com = com;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
+	@Override
+	public int total() {
+		//long start = System.currentTimeMillis();
+		
+		int result = this.kor + this.math + this.eng + this.com;
+		
+		try {
+			Thread.sleep(200);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		//long end = System.currentTimeMillis();
+		
+		//String message = (end - start) + "ms ½Ã°£";
+		//System.out.println(message);
+		return result;
+	}
+
+	@Override
+	public double avg() {
+		
+		double result = total() / 4.0d;
+		
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "NewlecExam [kor=" + kor + ", math=" + math + ", eng=" + eng + ", com=" + com + "]";
+	}
+
+}
