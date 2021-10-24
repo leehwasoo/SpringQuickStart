23d0318280b462111981da416650b89f84ab85f7
likelake13 <likelake13@naver.com>
2021-10-24 20:02:52 +0900

    [이화수 ] afterreturning, after throwing advice 사용

---
+++ spring/src/spring/aop/advice/LogAfterThrowingdvice.java
@@ -0,0 +1,10 @@
+package spring.aop.advice;
+
+import org.springframework.aop.ThrowsAdvice;
+
+public class LogAfterThrowingdvice implements ThrowsAdvice {
+	
+	public void afterThrowing(IllegalArgumentException e) throws Throwable {
+		System.out.println("예외가 발생했습니다. " + e.getMessage());
+	}
+}
