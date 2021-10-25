b2f8feb983afbe6cd70ad2b1ad81451703867c66
likelake13 <likelake13@naver.com>
2021-10-25 22:45:50 +0900

    [이화수] 강의자료로 제공된 html 파일을 활용한 홈페이지 초기 화면 index 구성

--- webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -12,10 +12,9 @@ public class IndexController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("index");
 		
 		mv.addObject("data", "hello spring mvc~");
-		mv.setViewName("/WEB-INF/view/index.jsp");
 		//mv.setViewName("WEB-INF/view/index.jsp");
 		
 		return mv;
